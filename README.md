b940f5f0300daf6918014c536c7b63d63bfcd191
erardlucien <erardlaurent1@gmail.com>
2024-04-06 03:41:22 +0200

    arrow-buttons and indicators were added.

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # touchswipe 
 
-## The website
+## The website-demo :point_down:
 
 https://erardlucien.github.io/touchswipe/
 
 The swipe work only on device with touchscreen.
+You can click the button or the indicators.
--- js/main.js
+++ js/main.js
@@ -4,20 +4,38 @@ const SLIDES = document.getElementsByClassName('slide')
 const NUMBER_SLIDE = SLIDES.length
 const SLIDER_CONTAINER = document.querySelector('.slider_container')
 const SLIDER = document.querySelector('.slider')
+const ARROW_LEFT = document.querySelector('.arrow-left')
+const ARROW_RIGHT = document.querySelector('.arrow-right')
 const SLIDE1_COPY = SLIDES[0].cloneNode(true)
 SLIDER.appendChild(SLIDE1_COPY)
 const NUMBER_SLIDE_NEW = NUMBER_SLIDE + 1
 SLIDER_CONTAINER.style.setProperty('--n', `${NUMBER_SLIDE_NEW}`)
 const TRANSITION = 'var(--transition)'
+const INDICATORS = document.querySelectorAll('.indicator')
+const FULL_INDICATOR_CLASS = 'indicator-full'
 
 let xStart= 0
 let yStart = 0
 let counter = 0
 let isGoingAtEnd = false
 let isGoingAtBegin = false
+let timeInterval = null
+let timeout = null
+
+function removeFullIndicatorClassOnPreviousIndicator() {
+    for(let i = 0; i < NUMBER_SLIDE; ++i) {
+        if(INDICATORS[i].classList.contains(FULL_INDICATOR_CLASS)) {
+            INDICATORS[i].classList.remove(FULL_INDICATOR_CLASS)
+        }
+    }
+}
 
 function slide() {
+    removeFullIndicatorClassOnPreviousIndicator()
     SLIDER.style.transform = `translateX(-${( 100/NUMBER_SLIDE_NEW ) * counter}%)`
+    if(counter > -1 && counter < NUMBER_SLIDE_NEW) {
+        INDICATORS[counter%NUMBER_SLIDE].classList.add(FULL_INDICATOR_CLASS);
+    }
 }
 
 function resetTransitionSLider() {
@@ -64,21 +82,31 @@ function slidingRight() {
 
 function goLeft() {
     ++counter
+
     if( counter === NUMBER_SLIDE_NEW ) {
         isGoingAtBegin = true
     }
+
     slidingLeft()
 }
 
 function goRight() {
     --counter
+
     if( counter === -1  ) {
         isGoingAtEnd = true
     }
+
     slidingRight()
 }
 
+function loop () {
+    goLeft()
+}
+
 SLIDER_CONTAINER.addEventListener('touchstart', (event) => {
+    clearInterval(timeInterval)
+    clearTimeout(timeout)
     event.preventDefault()
     xStart = event.changedTouches[0].screenX
     yStart = event.changedTouches[0].screenY
@@ -90,18 +118,54 @@ SLIDER_CONTAINER.addEventListener('touchend', (event) => {
     let yMovement = event.changedTouches[0].screenY
 
     // prevent a slide, when you scroll vertically
-    if( Math.abs(yMovement - yStart) / 10 > 6 ) {
+    if( Math.abs(yMovement - yStart) / 10 > 5 ) {
         return
     }
 
     if(xStart - xMovement < 0) {
         goRight()
-        return
-    }
-
-    if(xStart - xMovement > 0) {
+    } else if(xStart - xMovement > 0) {
         goLeft()
-        return
     }
 
+    timeout = setTimeout(activeLoop, 100)
 })
+
+for(let i = 0; i < NUMBER_SLIDE; ++i) {
+    INDICATORS[i].addEventListener('click', () => {
+        clearInterval(timeInterval)
+        clearTimeout(timeout)
+        if( counter === NUMBER_SLIDE - 1 && i === 0 ) {
+            goLeft()
+        } else if( counter === 0 && i === NUMBER_SLIDE - 1 ) {
+           goRight()
+        } else {
+            counter = i
+            slide()
+        }
+
+        timeout = setTimeout(activeLoop, 700)
+    })
+}
+
+ARROW_LEFT.addEventListener('click', () => {
+    clearInterval(timeInterval)
+    clearTimeout(timeout)
+    goRight()
+    timeout = setTimeout(activeLoop, 700)
+})
+
+ARROW_RIGHT.addEventListener('click', () => {
+    clearInterval(timeInterval)
+    clearTimeout(timeout)
+    goLeft()
+    timeout = setTimeout(activeLoop, 700)
+})
+
+function activeLoop() {
+    timeInterval = setInterval(() => {
+        loop()
+    }, 10000)
+}
+
+activeLoop()
