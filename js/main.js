799dedbbd0058e8124e142242249475272d4155d
erardlucien <erardlaurent1@gmail.com>
2024-04-07 17:33:52 +0200

    The condition for the prevention of scrolling was changed.

--- js/main.js
+++ js/main.js
@@ -116,7 +116,7 @@ SLIDER_CONTAINER.addEventListener('touchend', (event) => {
 
     // prevent a slide, as long
     // | xMovement - xStart | < 100
-    if( Math.abs(xMovement - xStart) < 100 ) {
+    if( Math.abs(xMovement - xStart) < 50 ) {
         timeout = setTimeout(activeLoop, 100)
         return
     }
